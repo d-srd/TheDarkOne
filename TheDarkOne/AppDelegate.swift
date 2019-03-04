ada7b31dd641c9f15b8594c62b2f8fcf7be3c782
Dino <dino.srdoc@degordian.com>
2019-03-04 19:06:57 +0100

    Initial Commit

---
+++ TheDarkOne/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TheDarkOne
+//
+//  Created by Dino on 04/03/2019.
+//  Copyright © 2019 bornfight. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
